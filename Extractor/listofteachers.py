d2003c6ab120a361f59b14a45c5f2c4bce6af565
amandesai01 <amandesai01@gmail.com>
2019-12-20 14:36:42 +0530

    Extractor Updated. Checked.

--- Extractor/listofteachers.py
+++ Extractor/listofteachers.py
@@ -6,6 +6,7 @@ def SendRequest():
     return r;
 
 def GetDepartment(i):
+    depart = 0
     try:
         if i['departments'][0]['DTO1'] == 'Mechanical Engineering':
             depart = 2
@@ -30,8 +31,9 @@ def GetAllTeachers():
     count = 0
     for i in r:
         Dict[count] = {'empid': i['empid'], 'name': i['name'], 'department': GetDepartment(i), 'email': i['email'],
-                       'position': i['position'], 'photo': "https://kjsce.somaiya.edu" + i['photo']}
+                       'position': i['position'], 'photo': "https://kjsce.somaiya.edu" + i['photo'], 'nameSearch' : i['name'].split(' ')}
         count += 1
+    # name also stored in array because querying in name is difficult for search purpose.
     return Dict
 
 def GetTeacher(empid):
@@ -39,9 +41,11 @@ def GetTeacher(empid):
     Dict = {}
     for i in r:
         if i['empid'] == empid:
-            Dict['data'] = {'empid': i['empid'], 'name': i['name'], 'department': GetDepartment(i), 'email': i['email'],'position': i['position'], 'photo': "https://kjsce.somaiya.edu" + i['photo']}
+            Dict['data'] = {'empid': i['empid'], 'name': i['name'], 'department': GetDepartment(i), 'email': i['email'],'position': i['position'], 'photo': "https://kjsce.somaiya.edu" + i['photo'], 'nameSearch' : i['name'].split(' ')}
             Dict['status'] = 'Success'
             return Dict
     Dict['data'] = 'No Response. Check Query'
     Dict['status'] = 'Fail'
     return Dict
+
+print(json.dumps(GetAllTeachers(), indent=4))
