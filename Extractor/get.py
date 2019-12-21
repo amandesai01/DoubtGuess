5af58b3a26b50f996ba478148c7f8e02cd13c7bc
amandesai01 <amandesai01@gmail.com>
2019-12-21 13:29:26 +0530

    Basic Retriever Ready. Filter to be made

---
+++ Extractor/get.py
@@ -0,0 +1,16 @@
+import requests
+import json
+def GetPayload(empid):
+    return {"target": "FACULTY", "targetid": empid, "isOwner": "False"}
+
+def GetData(apicall, empid):
+    return requests.post("https://kjsce.somaiya.edu/ajax/ajaxCall.aspx/"+apicall, json=GetPayload(empid)).json()
+
+def PersonalProfile(empid):
+    return GetData("GetPersonalProfile", empid)
+
+def Biography(empid):
+    return GetData("GetBiography", empid)
+
+def Publications(empid):
+    return GetData("GetPublications", empid)
