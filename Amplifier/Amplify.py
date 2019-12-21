a7b79dc25a7547427a7265c5cc1f1b414de24c48
amandesai01 <amandesai01@gmail.com>
2019-12-21 14:24:21 +0530

    Amplifier ready to amplify teacher data

---
+++ Amplifier/Amplify.py
@@ -0,0 +1,42 @@
+from bs4 import BeautifulSoup
+import json
+
+def filterString(strElement):
+    dataset = strElement.splitlines()
+    filteredDataset = [] #initiate empty list
+    for i in dataset:
+        temp = i.replace('\t','')
+        if i != '' and i != 'Undergraduate Courses' and i != 'Post Graduate Courses':
+            # print("Appended: " + i + " " + str(type(i)))
+            filteredDataset.append(temp)
+        # count = 0
+        # while count < len(filteredDataset):
+        #     if filteredDataset[count] == '\xa0':
+        #         filteredDataset.remove(count)
+        #     count += 1
+        # filteredDataset.remove('\\xa0')
+        # return filteredDataset
+
+    return filteredDataset
+
+def rawData(raw):
+    # print(raw)
+    raw = raw.get("d")
+    # print(type(raw))
+    raw = json.loads(raw)
+    # print(type(raw))
+    rawDict = {}  # initiate empty dictionary
+    for i in raw:
+        key = i["DTO1"]
+        try:
+            soup = BeautifulSoup(i["CommonList"][0]["DTO1"], 'html.parser')
+            dataToken = soup.get_text()
+            dataset = filterString(dataToken)
+            value = dataset
+        except IndexError:  # This specifies that specific teacher has nothing to showoff in that section
+            value = []
+        rawDict[key] = value
+    return rawDict
+
+def filterpersonalprofile(data):
+    return rawData(data)
